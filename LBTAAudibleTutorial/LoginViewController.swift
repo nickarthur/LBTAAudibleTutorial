032b75782155d6d210094e4cb3f6f7a7abd79d82
Kevin Quisquater <quisquater.kevin@gmail.com>
2017-06-17 17:23:00 +0200

    Add a login page implement current page dot color in page control

--- LBTAAudibleTutorial/LoginViewController.swift
+++ LBTAAudibleTutorial/LoginViewController.swift
@@ -23,12 +23,27 @@ class LoginViewController: UIViewController, UICollectionViewDelegate, UICollect
     }()
     
     let cellId = "cellId"
+    let loginCellId = "loginCellId"
     
     let pages: [Page] = {
-        let firstPage = Page(title: Constants.Tutorials.Titles.tutorial1, message: Constants.Tutorials.Texts.tutorial1, imageName: Constants.Tutorials.ImageNames.tutorial1)
-        let secondPage = Page(title: Constants.Tutorials.Titles.tutorial2, message: Constants.Tutorials.Texts.tutorial2, imageName: Constants.Tutorials.ImageNames.tutorial2)
-        let thirdPage = Page(title: Constants.Tutorials.Titles.tutorial3, message: Constants.Tutorials.Texts.tutorial3, imageName: Constants.Tutorials.ImageNames.tutorial3)
+        let firstPage = Page(title: Constants.Tutorials.Titles.tutorial1,
+                             message: Constants.Tutorials.Texts.tutorial1,
+                             imageName: Constants.Tutorials.ImageNames.tutorial1)
+        let secondPage = Page(title: Constants.Tutorials.Titles.tutorial2,
+                              message: Constants.Tutorials.Texts.tutorial2,
+                              imageName: Constants.Tutorials.ImageNames.tutorial2)
+        let thirdPage = Page(title: Constants.Tutorials.Titles.tutorial3,
+                             message: Constants.Tutorials.Texts.tutorial3,
+                             imageName: Constants.Tutorials.ImageNames.tutorial3)
         return [firstPage, secondPage, thirdPage]
+    }()
+    
+    lazy var pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.pageIndicatorTintColor = .lightGray
+        pc.currentPageIndicatorTintColor = Constants.Colors.orange
+        pc.numberOfPages = self.pages.count + 1
+        return pc
     }()
     
     let skipButton: UIButton = {
@@ -49,21 +64,40 @@ class LoginViewController: UIViewController, UICollectionViewDelegate, UICollect
         super.viewDidLoad()
         
         view.addSubview(collectionView)
+        view.addSubview(pageControl)
         view.addSubview(skipButton)
         view.addSubview(nextButton)
+        
+        pageControl.anchor(nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 40)
         
         skipButton.anchor(view.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, topConstant: 16, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 60, heightConstant: 50)
         nextButton.anchor(view.topAnchor, left: nil, bottom: nil, right: view.rightAnchor, topConstant: 16, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 60, heightConstant: 50)
         
         collectionView.fillSuperview()
+        registerCells()
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+        
+        pageControl.currentPage = Int(targetContentOffset.pointee.x / view.frame.width)
+    }
+    
+    fileprivate func registerCells() {
         collectionView.register(PageCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: loginCellId)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return pages.count
+        return pages.count + 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.item == pages.count {
+            let loginCell = collectionView.dequeueReusableCell(withReuseIdentifier: loginCellId, for: indexPath)
+            return loginCell
+        }
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! PageCell
         
         let page = pages[indexPath.row]
