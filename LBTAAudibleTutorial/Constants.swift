0535e0465c9b06b1d6d1392f44ee4140622721b9
Kevin Quisquater <quisquater.kevin@gmail.com>
2017-06-17 11:28:48 +0200

    Add a collection view with 4 full-page cells

---
+++ LBTAAudibleTutorial/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  LBTAAudibleTutorial
+//
+//  Created by Kevin Quisquater on 17/06/2017.
+//  Copyright © 2017 Kevin Quisquater. All rights reserved.
+//
+
+struct Constants {
+    struct ImageNames {
+        static let tutorial1 = "page1"
+    }
+}
---
+++ LBTAAudibleTutorial/PageCell.swift
@@ -0,0 +1,37 @@
+//
+//  PageCell.swift
+//  LBTAAudibleTutorial
+//
+//  Created by Kevin Quisquater on 17/06/2017.
+//  Copyright © 2017 Kevin Quisquater. All rights reserved.
+//
+
+import UIKit
+
+class PageCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+    }
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.image = UIImage(named: Constants.ImageNames.tutorial1)
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    func setupViews() {
+        
+        addSubview(imageView)
+        imageView.anchorToTop(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
