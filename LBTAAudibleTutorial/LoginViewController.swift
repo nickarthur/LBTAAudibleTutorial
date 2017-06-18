95cdde6fe653e11d3a0283f56ea1903efc9837fe
Kevin Quisquater <quisquater.kevin@gmail.com>
2017-06-18 13:54:26 +0200

    Add support for landscape orientation

--- LBTAAudibleTutorial/LoginViewController.swift
+++ LBTAAudibleTutorial/LoginViewController.swift
@@ -109,7 +109,8 @@ class LoginViewController: UIViewController, KeyboardNotificable, UICollectionVi
     
     func keyboardWillShow(notification: Notification) {
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: { 
-            self.view.frame = CGRect(x: 0, y: -50, width: self.view.frame.width, height: self.view.frame.height)
+            let y: CGFloat = UIDevice.current.orientation.isLandscape ? -100 : -50
+            self.view.frame = CGRect(x: 0, y: y, width: self.view.frame.width, height: self.view.frame.height)
         }, completion: nil)
     }
     
@@ -188,5 +189,27 @@ class LoginViewController: UIViewController, KeyboardNotificable, UICollectionVi
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: view.frame.height)
     }
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        // Redraw the collectionView with proper width and height for each cell
+        collectionView.collectionViewLayout.invalidateLayout()
+        let indexPath = IndexPath(item: pageControl.currentPage, section: 0)
+        
+        // Scroll to indexPath AFTER the rotation is done
+        DispatchQueue.main.async {
+            self.collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            self.collectionView.reloadData()
+            
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
 
